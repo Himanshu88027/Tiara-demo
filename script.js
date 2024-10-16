c4d5e32f953cea3c610b9fd247df1e989d0c4438
Himanshu88027 <himanshukumar88027@gmail.com>
2024-10-16 16:48:30 +0530

    testing

--- script.js
+++ script.js
@@ -10,27 +10,3 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
-
-$(document).ready(function(){
-    $('.card-carousel').slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        dots: true,
-        responsive: [
-            {
-                breakpoint: 992,
-                settings: {
-                    slidesToShow: 2
-                }
-            },
-            {
-                breakpoint: 576,
-                settings: {
-                    slidesToShow: 1
-                }
-            }
-        ]
-    });
-});
